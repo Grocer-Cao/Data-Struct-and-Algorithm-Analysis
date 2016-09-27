329d87c796a0a7925522f8b10dbf0bf4c7ed995d
Filter <cao_penghui@126.com>
2016-09-28 00:18:01 +0800

    增加了快速排序
    增加了快速排序

--- Sequence/QuickSort.c
+++ Sequence/QuickSort.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int * num_addr;
+//int * num_addr;
 
 void prt_array(int* arry, int size)
 {
@@ -73,6 +73,29 @@ void quickSort(int* arry, int size)
     else;
 }
 
+//参考：http://www.cnblogs.com/luchen927/archive/2012/02/29/2368070.html
+int quicksort_1 (int* v, int left, int right)
+{
+        if(left < right){
+                int key = v[left];
+                int low = left;
+                int high = right;
+                while(low < high){
+                        while(low < high && v[high] > key){
+                                high--;
+                        }
+                        v[low] = v[high];
+                        while(low < high && v[low] <= key){
+                                low++;
+                        }
+                        v[high] = v[low];
+                }
+                v[low] = key;
+                quicksort_1(v,left,low-1);
+                quicksort_1(v,low+1,right);
+        }
+        return 0;
+}
 
 
 int main()
@@ -82,10 +105,15 @@ int main()
     //int num[11] = {10,9,8,7,6,5,4,3,2,1,0};
     //int num[11] = {10,8,2,4,2,7,5,5,0,9,1};
     //int num[11] = {2,2,2,2,2,2,2,2,2,2,2};
-    //int num[11] = {10,8,-2,4,3,-7,6,2555,0,9,11};
-    num_addr = num;
+    //int num[11] = {10,8,-2,3,3,-7,6,2555,0,9,11};
+
+    //num_addr = num;
+
     prt_array(num,sizeof(num)/sizeof(int));
-    quickSort(num, sizeof(num)/sizeof(int));
+
+    //quickSort(num, sizeof(num)/sizeof(int));
+    quicksort_1(num,0,11);
+
     prt_array(num,sizeof(num)/sizeof(int));
     return 0;
 }
